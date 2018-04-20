2879dcb8fd9cac151d8594b5d300bfda3ac3a479
Daphne <a.milky.sky@gmail.com>
2018-04-20 15:47:09 -0700

    Improve naively implemented getDisplayDate helper function

--- src/utils/index.js
+++ src/utils/index.js
@@ -40,7 +40,24 @@ export const getDisplayDate = (timestamp) => {
 
     // TODO: Format the date like: "Tuesday, April 11, 2017"
 
-    return date.toString();
+    let days = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday'
+    ];
+
+    let months = [
+      'January', 'February', 'March',
+      'April', 'May', 'June', 'July',
+      'August', 'September', 'October',
+      'November', 'December'
+    ];
+
+    let day = date.getDay();
+    let month = date.getMonth();
+    let dateInMonth = date.getDate();
+    let year = date.getFullYear();
+
+    return `${days[day]}, ${months[month]} ${dateInMonth}, ${year}`;
 };
 
 /**
